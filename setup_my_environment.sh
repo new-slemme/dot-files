61ad426de172e5933385d8ef289302aebb0de5dd
Tim Dahlem <t.dahlem@mtx.int@motodox.de>
2026-03-25 15:19:38 +0100

    setup

--- setup_my_environment.sh
+++ setup_my_environment.sh
@@ -5,7 +5,9 @@ cp -rv ./ ~/.config/
 cp ~/.config/fish/functions/fish_greeting.fish.ssh ~/.config/fish/functions/fish_greeting.fish
 cp ~/.config/neofetch/config_base.conf ~/.config/neofetch/config.conf
 
-sudo apt install neofetch exa ncdu bat zoxide fzf fd-find playerctl python3-dev python3-pip python3-setuptools thefuck
+sudo apt install neofetch exa ncdu bat zoxide fzf fd-find playerctl thefuck
+
+#python3-dev python3-pip python3-setuptools
 
 curl -LO https://github.com/ClementTsang/bottom/releases/download/0.12.3/bottom_0.12.3-1_amd64.deb
 sudo dpkg -i bottom_0.12.3-1_amd64.deb
