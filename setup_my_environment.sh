60385370a46e94ea2ac5527648b3a1e02db73172
Tim Dahlem <t.dahlem@mtx.int@motodox.de>
2026-03-25 14:58:49 +0100

    setup

--- setup_my_environment.sh
+++ setup_my_environment.sh
@@ -1,5 +1,19 @@
-sudo apt install fish neofetch exa ncdu bat
+#!/usr/bin/fish
+
+cp ./* ~/.config/
+
+sudo apt install neofetch exa ncdu bat zoxide fzf fd-find
+
+curl -LO https://github.com/ClementTsang/bottom/releases/latest/download/bottom_x86_64-unknown-linux-gnu.deb
+sudo dpkg -i bottom_x86_64-unknown-linux-gnu.deb
+
+abbr -a fd fdfind
+thefuck --alias >> ~/.config/fish/config.fish
+echo "zoxide init fish | source" > ~/.config/fish/config.fish
+
 curl -sS https://starship.rs/install.sh | sh
-starship init fish | source
-echo "starship init fish | source" > .config/fish/config.fish
+echo "starship init fish | source" > ~/.config/fish/config.fish
+
+source ~/.config/fish/config.fish
+
 fish_greeting
