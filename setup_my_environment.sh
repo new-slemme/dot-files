f32161fb6ed8a6f9a7889679759722be40f2d1ef
Tim Dahlem <t.dahlem@mtx.int@motodox.de>
2026-03-25 15:01:44 +0100

    setup

--- setup_my_environment.sh
+++ setup_my_environment.sh
@@ -2,7 +2,9 @@
 
 cp -rv ./* ~/.config/
 
-sudo apt install neofetch exa ncdu bat zoxide fzf fd-find
+sudo apt install neofetch exa ncdu bat zoxide fzf fd-find playerctl python3-dev python3-pip python3-setuptools 
+
+pip3 install thefuck --user
 
 curl -LO https://github.com/ClementTsang/bottom/releases/latest/download/bottom_x86_64-unknown-linux-gnu.deb
 sudo dpkg -i bottom_x86_64-unknown-linux-gnu.deb
