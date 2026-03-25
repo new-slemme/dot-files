41b572054e2bd7edbbdea672739dead7ed1454d9
Tim Dahlem <t.dahlem@mtx.int@motodox.de>
2026-03-25 15:04:06 +0100

    setup

--- setup_my_environment.sh
+++ setup_my_environment.sh
@@ -2,6 +2,8 @@
 
 cp -rv ./* ~/.config/
 
+cp ~/.config/fish/functions/fish_greeting.fish.ssh ~/.config/fish/functions/fish_greeting.fish
+
 sudo apt install neofetch exa ncdu bat zoxide fzf fd-find playerctl python3-dev python3-pip python3-setuptools 
 
 pip3 install thefuck --user
@@ -11,9 +13,11 @@ sudo dpkg -i bottom_x86_64-unknown-linux-gnu.deb
 
 abbr -a fd fdfind
 thefuck --alias >> ~/.config/fish/config.fish
+zoxide init fish | source
 echo "zoxide init fish | source" > ~/.config/fish/config.fish
 
 curl -sS https://starship.rs/install.sh | sh
+starship init fish | source
 echo "starship init fish | source" > ~/.config/fish/config.fish
 
 source ~/.config/fish/config.fish
